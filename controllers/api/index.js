981ea6352ccb36c0ff361bb6438a248e35ed30d2
Gene <genedanielkim@gmail.com>
2023-03-01 18:49:48 -0800

    Redirect user to login when they hit Profile without being logged in

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,10 @@ const userRoutes = require('./userRoutes');
 const logRoutes = require('./logRoutes');
 const workoutRoutes = require('./workoutRoutes');
 
+
 router.use('/users', userRoutes);
 router.use('/log', logRoutes);
 router.use('/workout', workoutRoutes);
+
 
 module.exports = router;
