3e4df9f03219ef32a39843d004c73598606f80d8
kenghoufong1 <kenghoufong1@gmail.com>
2023-03-06 20:24:52 -0800

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,10 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
- {
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -10,7 +13,7 @@ let sequelize;
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3001
+      port: 3306
     }
   );
 }
